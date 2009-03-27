95e8087f4f8bd7eea2f91ac8bfe236d26e652d2e
Otto Hilska <otto.hilska@nodeta.fi>
2009-03-27 20:07:26 +0200

    action_controller was not needed after all.

--- test/unit/legacy_smart_session_test.rb
+++ test/unit/legacy_smart_session_test.rb
@@ -1,7 +1,5 @@
 require File.join(File.dirname(__FILE__), '../test_helper')
 
-require 'action_controller'
-
 Struct.new 'CGISession', :session_id
 class LegacySmartSessionTest < ActiveSupport::TestCase
   fixtures :sessions
